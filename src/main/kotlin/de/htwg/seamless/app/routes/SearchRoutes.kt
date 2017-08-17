999c8d56cc26cdb97791a01af56c51926bb6a817
Tobias Keh <tobias.keh@googlemail.com>
2017-08-17 20:14:31 +0200

    refactoring

--- src/main/kotlin/de/htwg/seamless/app/routes/SearchRoutes.kt
+++ src/main/kotlin/de/htwg/seamless/app/routes/SearchRoutes.kt
@@ -4,14 +4,14 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import de.htwg.seamless.app.domain.Tool
 import de.htwg.seamless.app.mapper
 import de.htwg.seamless.app.util.SearchKeywords
-import spark.Spark
+import spark.Spark.*
 
 class SearchRoutes {
 
     init { initializeRoutes() }
 
     fun initializeRoutes() {
-        Spark.get("/search") { req, _ ->
+        get("/search") { req, _ ->
             val searchKeywords: SearchKeywords = mapper.readValue<SearchKeywords>(req.body())
             val tools = mutableListOf<Tool>()
             searchKeywords.properties.forEach { tools.addAll(Tool.where().properties.name.contains(it).findList()) }
