270bcda5e23f32d28fbad8f5c57ade0e622a951d
Tobias Keh <tobias.keh@googlemail.com>
2017-08-17 16:50:50 +0200

    Search routes extracted

--- src/main/kotlin/de/htwg/seamless/app/App.kt
+++ src/main/kotlin/de/htwg/seamless/app/App.kt
@@ -5,15 +5,14 @@ import com.fasterxml.jackson.databind.JsonMappingException
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
-import de.htwg.seamless.app.domain.Tool
 import de.htwg.seamless.app.routes.DimensionRoutes
 import de.htwg.seamless.app.routes.PropertyRoutes
+import de.htwg.seamless.app.routes.SearchRoutes
 import de.htwg.seamless.app.routes.ToolRoutes
 import de.htwg.seamless.app.util.Util
-import de.htwg.seamless.app.util.SearchKeywords
 import io.ebean.EbeanServerFactory
 import io.ebean.config.ServerConfig
+import spark.Spark
 import spark.Spark.*
 import java.io.FileInputStream
 import java.io.IOException
@@ -29,39 +28,31 @@ fun main(args: Array<String>) {
     // Ebean configuration
     val properties: Properties = Properties()
     properties.load(FileInputStream("src/main/resources/ebean.properties"))
-    val config: ServerConfig = ServerConfig()
+    val config = ServerConfig()
     config.name = "tool_map"
     config.isDefaultServer = true
     config.loadFromProperties(properties)
     EbeanServerFactory.create(config)
 
     // Spark routes initialization
+    staticFiles.location("/static")
     ToolRoutes()
     DimensionRoutes()
     PropertyRoutes()
-
-    // Tool search route
-    get("/search") { req, _ ->
-        val searchKeywords: SearchKeywords = mapper.readValue<SearchKeywords>(req.body())
-        val tools = mutableListOf<Tool>()
-        searchKeywords.properties.forEach { tools.addAll(Tool.where().properties.name.contains(it).findList()) }
-        searchKeywords.dimensions.forEach { tools.addAll(Tool.where().properties.dimensions.name.contains(it).findList()) }
-
-        mapper.writeValueAsString(tools)
-    }
+    SearchRoutes()
 
     // Exception handling
-    exception(JsonParseException::class.java, { _, _, response ->
+    Spark.exception(JsonParseException::class.java, { _, _, response ->
         response.status(500)
         response.body("JSON parsing failed")
     })
 
-    exception(JsonMappingException::class.java, { _, _, response ->
+    Spark.exception(JsonMappingException::class.java, { _, _, response ->
         response.status(500)
         response.body("JSON mapping failed")
     })
 
-    exception(IOException::class.java, { _, _, response ->
+    Spark.exception(IOException::class.java, { _, _, response ->
         response.status(500)
         response.body("Processing of request failed")
     })
---
+++ src/main/kotlin/de/htwg/seamless/app/routes/SearchRoutes.kt
@@ -0,0 +1,23 @@
+package de.htwg.seamless.app.routes
+
+import com.fasterxml.jackson.module.kotlin.readValue
+import de.htwg.seamless.app.domain.Tool
+import de.htwg.seamless.app.mapper
+import de.htwg.seamless.app.util.SearchKeywords
+import spark.Spark
+
+class SearchRoutes {
+
+    init { initializeRoutes() }
+
+    fun initializeRoutes() {
+        Spark.get("/search") { req, _ ->
+            val searchKeywords: SearchKeywords = mapper.readValue<SearchKeywords>(req.body())
+            val tools = mutableListOf<Tool>()
+            searchKeywords.properties.forEach { tools.addAll(Tool.where().properties.name.contains(it).findList()) }
+            searchKeywords.dimensions.forEach { tools.addAll(Tool.where().properties.dimensions.name.contains(it).findList()) }
+
+            mapper.writeValueAsString(tools)
+        }
+    }
+}
