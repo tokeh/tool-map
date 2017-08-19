2b3a72a5898c9711b42a1b2397658eea5f45589c
Tobias Keh <tobias.keh@googlemail.com>
2017-08-19 21:14:34 +0200

    Properties partial template added

--- src/main/kotlin/de/htwg/seamless/app/routes/PageRoutes.kt
+++ src/main/kotlin/de/htwg/seamless/app/routes/PageRoutes.kt
@@ -1,6 +1,7 @@
 package de.htwg.seamless.app.routes
 
 import de.htwg.seamless.app.domain.Dimension
+import de.htwg.seamless.app.domain.Property
 import de.htwg.seamless.app.domain.Tool
 import de.htwg.seamless.app.util.TemplateDataContainer
 import spark.ModelAndView
@@ -13,7 +14,7 @@ class PageRoutes {
 
     private fun initializeRoutes() {
         get("/toolMap") { _, _ ->
-            val data = TemplateDataContainer(Tool.all(), Dimension.all(), Tool.all())
+            val data = TemplateDataContainer(Tool.all(), Dimension.all(), Property.all(), Tool.all())
 
             HandlebarsTemplateEngine().render(
                     ModelAndView(data, "map_template.hbs")
--- src/main/kotlin/de/htwg/seamless/app/util/TemplateDataContainer.kt
+++ src/main/kotlin/de/htwg/seamless/app/util/TemplateDataContainer.kt
@@ -1,12 +1,15 @@
 package de.htwg.seamless.app.util
 
 import de.htwg.seamless.app.domain.Dimension
+import de.htwg.seamless.app.domain.Property
 import de.htwg.seamless.app.domain.Tool
 
 data class TemplateDataContainer(
         val tools: List<Tool>,
 
         val dimensions: List<Dimension>,
+
+        val properties: List<Property>,
 
         val favourites: List<Tool>
 )
