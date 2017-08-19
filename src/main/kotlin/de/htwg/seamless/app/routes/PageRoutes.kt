f820d490b082628c94cb04f5ab6067f8d6fc4f64
Tobias Keh <tobias.keh@googlemail.com>
2017-08-19 13:36:48 +0200

    refactoring

--- src/main/kotlin/de/htwg/seamless/app/routes/PageRoutes.kt
+++ src/main/kotlin/de/htwg/seamless/app/routes/PageRoutes.kt
@@ -1,11 +1,23 @@
 package de.htwg.seamless.app.routes
 
+import spark.ModelAndView
+import spark.Spark.*
+import spark.template.handlebars.HandlebarsTemplateEngine
+
 class PageRoutes {
 
     init { initializeRoutes() }
 
-    fun initializeRoutes() {
+    private fun initializeRoutes() {
+        get("/toolMap") { _, _ ->
+            val jsonModel = ""
 
+
+
+            HandlebarsTemplateEngine().render(
+                    ModelAndView(jsonModel, "/map_template.hbs")
+            )
+        }
     }
 
 }
