d8fb7017211be638ca0d528af36dd198082251da
Tobias Keh <tobias.keh@googlemail.com>
2017-08-22 20:17:09 +0200

    Filter queries added (still with lateinit not initialized exception)

--- src/main/kotlin/de/htwg/seamless/app/routes/FilterRoutes.kt
+++ src/main/kotlin/de/htwg/seamless/app/routes/FilterRoutes.kt
@@ -1,23 +1,42 @@
 package de.htwg.seamless.app.routes
 
 import com.fasterxml.jackson.module.kotlin.readValue
+import de.htwg.seamless.app.domain.Dimension
 import de.htwg.seamless.app.domain.Tool
 import de.htwg.seamless.app.mapper
-import de.htwg.seamless.app.util.SearchKeywords
+import de.htwg.seamless.app.util.FilterKeywords
+import de.htwg.seamless.app.util.TemplateDataContainer
+import spark.ModelAndView
 import spark.Spark.*
+import spark.template.handlebars.HandlebarsTemplateEngine
 
-class SearchRoutes {
+class FilterRoutes {
 
     init { initializeRoutes() }
 
     private fun initializeRoutes() {
-        get("/search") { req, _ ->
-            val searchKeywords: SearchKeywords = mapper.readValue(req.body())
-            val tools: MutableList<Tool> = mutableListOf()
-            searchKeywords.properties.forEach { tools.addAll(Tool.where().properties.name.contains(it).findList()) }
-            searchKeywords.dimensions.forEach { tools.addAll(Tool.where().properties.dimensions.name.contains(it).findList()) }
+        post("/filter") { req, _ ->
+            val filterKeywords: FilterKeywords = mapper.readValue(req.body())
 
-            mapper.writeValueAsString(tools)
+            val tools: MutableSet<Tool> = mutableSetOf()
+
+            filterKeywords.toolNames.forEach {
+                Tool.where().toolName.contains(it).findList()
+            }
+
+            filterKeywords.properties.forEach {
+                Tool.where().properties.name.contains(it).findList()
+            }
+
+            filterKeywords.dimensions.forEach {
+                Tool.where().properties.dimensions.name.contains(it).findList()
+            }
+
+            val dimensions = Dimension.where().index.asc().findList()
+            val data = TemplateDataContainer(tools = tools.toList().map { it.updateToolData() }, dimensions = dimensions)
+
+            println(HandlebarsTemplateEngine().render(
+                    ModelAndView(data, "map_template.hbs")))
         }
     }
 }
