778b0442cf1d52333b91932e08537292a94eab2a
Tobias Keh <tobias.keh@googlemail.com>
2017-08-22 17:44:22 +0200

    Favourites correctly implemented

--- src/main/kotlin/de/htwg/seamless/app/routes/PageRoutes.kt
+++ src/main/kotlin/de/htwg/seamless/app/routes/PageRoutes.kt
@@ -15,8 +15,15 @@ class PageRoutes {
     private fun initializeRoutes() {
         get("/") { _, _ ->
 
+            var favourites = Tool.query().select("rating").orderBy("rating").findList()
+
+            favourites = when (favourites.size < 3) {
+                true -> favourites.subList(0, favourites.size)
+                false -> favourites.subList(0, 3)
+            }
+
             val data = TemplateDataContainer(hashMapOf(Pair("home", true)),
-                    Tool.all().map { it.updateToolData() }, Dimension.all(), Property.all(), Tool.all())
+                    Tool.all().map { it.updateToolData() }, Dimension.all(), Property.all(), favourites)
 
             HandlebarsTemplateEngine().render(
                     ModelAndView(data, "map_template.hbs")
